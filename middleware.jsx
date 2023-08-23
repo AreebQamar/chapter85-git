1c0c387c9acb680af85e3963912df21e7ba443fc
AreebQamar <areeb.qamar1225@gmail.com>
2023-08-23 15:40:38 +0500

    Update middleware.jsx

--- middleware.jsx
+++ middleware.jsx
@@ -1,6 +1,6 @@
-//export { default } from "next-auth/middleware";
+export { default } from "next-auth/middleware";
 
 //export const config = { matcher: [] }
-//export const config = { matcher: ["/admin", "/admin/orders", "/admin/addProducts", "/checkOut", "/manageAccount"] }
+export const config = { matcher: ["/admin", "/admin/orders", "/admin/addProducts", "/checkOut", "/manageAccount"] }
 //export const config = { matcher: ['/((?!login|register|admin|checkOut|manageAccount).*)'] }
 

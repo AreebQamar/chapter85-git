2b643cbdf154f30230cb1a484888d53cab9e17cd
Areeb Qamar <areeb.qamar1225@gmail.com>
2024-04-15 00:27:15 +0500

    about us page

---
+++ app/about-us/page.jsx
@@ -0,0 +1,12 @@
+
+
+export default function aboutUsPage() {
+    return (
+        <div>
+            check out
+            <a target="_blank" rel="noopener noreferrer" href="https://areebqamar.vercel.app">
+                <h1 className="text-xl font-bold">my portfolio</h1>
+            </a>
+        </div>
+    )
+}

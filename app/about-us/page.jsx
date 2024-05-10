dc6840b8e5ec9f484d7398a22ff7a7ee676a8d8c
Areeb Qamar <areeb.qamar1225@gmail.com>
2024-05-10 16:59:57 +0500

    about-us page

--- app/about-us/page.jsx
+++ app/about-us/page.jsx
@@ -2,10 +2,12 @@
 
 export default function aboutUsPage() {
     return (
-        <div>
-            check out
+        <div className="flex items-center text-xl font-bold">
+            <div className="items-center mr-2">
+                check out
+            </div>
             <a target="_blank" rel="noopener noreferrer" href="https://areebqamar.vercel.app">
-                <h1 className="text-xl font-bold">my portfolio</h1>
+                <h1 className="text-blue-500 underline">my portfolio</h1>
             </a>
         </div>
     )
